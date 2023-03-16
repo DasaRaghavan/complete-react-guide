5ebb8196ff5ae44491802fda66c21886afb414d6
dasar <dashfamily@gmail.com>
2023-03-15 20:10:17 -0400

    New Expense and expense forms event handlers

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -4,8 +4,19 @@ import '../css/ExpenseForm.css'
 const ExpenseForm = () => {
     return <div>
         <form>
-            <div>
-                <input type='text' />
+            <div className='new-expense__controls'>
+                <div className='new-expense__control'>
+                    <label>Title</label>
+                    <input type='text' />
+                </div>
+                <div className='new-expense__control'>
+                    <label>Expense Amount</label>
+                    <input type='number' min='0.01' step='0.01'/>
+                </div>
+                <div className='new-expense__control'>
+                    <label>Expense Date</label>
+                    <input type='date' min='2021-01-01' max='2023-12-31' />
+                </div>
             </div>
 
         </form>
