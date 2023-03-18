3bfa514c39d10894c0409c64fcb10bb40fc1c7d2
dasar <dashfamily@gmail.com>
2023-03-18 12:07:44 -0400

    comments

--- src/components/NewExpenses/NewExpense.js
+++ src/components/NewExpenses/NewExpense.js
@@ -3,12 +3,14 @@ import ExpenseForm from './ExpenseForm.js'
 import '../css/NewExpense.css'
 
 const NewExpense = (props) => {
+    // this is called using the props within the child ExpenseForm.js
     const addExpenseHandler = (expenseData) => {
         const newExpenseData = {
             ...expenseData,
             id: Math.random().toString()
         }
-        props.onNewExpense(newExpenseData) //send object to parent (App.js)
+        // onNewExpense is a prop defined in the parent App.js
+        props.onNewExpense(newExpenseData) //props is used the send object to parent (App.js)
     }
 
     return <div className='new-expense'>
